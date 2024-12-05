d3d35df8aa9a75163ffb50dfd55c779f21e600e1
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-04 21:24:40 -0500

    feat( create first css module)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
-
+import styles from "./App.module.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className={styles.title}>Buscador del clima</h1>
+      <div className={styles.container}>
+        <input
+          type="text"
+          placeholder="Ciudad"
+          className={styles.input}
+        />
+        <button className={styles.button}>Buscar</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
